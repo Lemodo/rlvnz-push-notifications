de5260ee98ba74e082975d77959c848be3390436
Leo <45721247+Lemodo@users.noreply.github.com>
2022-01-24 15:45:19 +0100

    keys / notification update

--- worker.js
+++ worker.js
@@ -4,7 +4,9 @@ self.addEventListener('push', ev => {
   const data = ev.data.json();
   console.log('Got push', data);
   self.registration.showNotification(data.title, {
-    body: 'monkee',
-    icon: 'https://img.posterlounge.de/images/big/1868016.jpg'
+    body: 'Hello World!',
+    icon: 'https://img.posterlounge.de/images/big/1868016.jpg',
+    vibrate:[200, 100, 200, 100, 200, 100, 400]
+
   });
 });
