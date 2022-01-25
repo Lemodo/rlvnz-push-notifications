1f59b998bace69ea90c0df6025bf20bc21b31132
Leo <45721247+Lemodo@users.noreply.github.com>
2022-01-25 14:54:39 +0100

    sendNotification should work now, but it doesn't :)

--- firebase/routes/token_routes.js
+++ firebase/routes/token_routes.js
@@ -6,7 +6,6 @@ var tokenRouter = module.exports = exports = express.Router();
 
 // Simple route to accept token from user
 tokenRouter.post('/setToken', jsonParser, (req, res) => {
-    let data = JSON.stringify(req.body.token);
-    fs.writeFileSync("tokens.json", data);  //! write the file somewhere here 
-    console.log('TOKEN RECEIVED: ' + req.body.token);
+    fs.writeFileSync("tokens.json", req.body.token, { flags:'a+' });
+    console.log("TOKEN: " + req.body.token);
 });
---
+++ firebase/sendNotification.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const request = require('request');
+
+
+let rawdata = fs.readFileSync('tokens.json');
+const requestData = JSON.parse(rawdata);
+
+
+const headers = {
+    'Authorization': 'key=AAAAD3rw-Rg:APA91bG4zeV4RiSqlR7r5Xv-RCrXkHZ4zp9d0X8_X19ZZtZvLZlQXTQEgxAhB6OXvXwCWxPAXjfY2Y5coE8E4ROz0KBCjYyDGQiwo8WGZQr15NmrbWUkTUpihieNFWJOcdUOqHYz79k4',
+    'Content-Type': 'application/json'
+};
+
+const dataString = "{\"to\":\""+requestData+"\",\"data\":{\"notification\":{\"body\":\"Are you coming to our party?\",\"title\":\"This is a tester tester\",\"confirm\":\"https://developers.google.com/web/\",\"decline\":\"https://www.yahoo.com/\"}},\"priority\":10}"
+
+const options = {
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+request(options, callback);
+
+
+
+
+//?curl POST --Header "Authorization: key=AAAAD3rw-Rg:APA91bG4zeV4RiSqlR7r5Xv-RCrXkHZ4zp9d0X8_X19ZZtZvLZlQXTQEgxAhB6OXvXwCWxPAXjfY2Y5coE8E4ROz0KBCjYyDGQiwo8WGZQr15NmrbWUkTUpihieNFWJOcdUOqHYz79k4" --Header "Content-Type:application/json" https://fcm.googleapis.com/fcm/send -d "{\"to\":\"fd-H4ZVr0jQ:APA91bETET_IFr4hfH5GRURWHN4V6EhC8OkRNwb8hzh2iUGF3Z8-jNDtT8Ei6vpSeWYV0B0DESrDCkbNT54NDXaYi6B2qNABLz1HUL39YYvGL-QT7Pbtnwp10TDa8wq2v3Y47JLI7fl_\",\"data\":{\"notification\":{\"body\":\"Are you coming to our party?\",\"title\":\"This is a tester tester\",\"confirm\":\"https://developers.google.com/web/\",\"decline\":\"https://www.yahoo.com/\"}},\"priority\":10}"
