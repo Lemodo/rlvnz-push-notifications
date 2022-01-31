7de03c04738337cb7b6a07f095badf09c41c2676
Leo <45721247+Lemodo@users.noreply.github.com>
2022-01-31 09:22:38 +0100

    send topic button

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 const tokenRouter = require(__dirname + '/routes/token_routes');
 
+
+//Send notification button
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.post('/api/notificationSend', function(req, res){
   const requestData = req.body.token;
@@ -41,6 +43,44 @@ app.post('/api/notificationSend', function(req, res){
   request(options, callback);
 
 });
+
+
+//send topic button
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.post('/api/topicSend', function(req, res){
+  const requestData = req.body.topic;
+  const title = req.body.title;
+  const message = req.body.message;
+  const confirmLink = req.body.accept;
+  const declineLink = req.body.deny;
+  res.send("recieved request!");
+
+  const headers = {
+    'Authorization': 'key=AAAAD3rw-Rg:APA91bG4zeV4RiSqlR7r5Xv-RCrXkHZ4zp9d0X8_X19ZZtZvLZlQXTQEgxAhB6OXvXwCWxPAXjfY2Y5coE8E4ROz0KBCjYyDGQiwo8WGZQr15NmrbWUkTUpihieNFWJOcdUOqHYz79k4',
+    'Content-Type': 'application/json'
+  };
+
+  console.log("sending to:" + requestData);
+  const dataString = "{\"to\":\"/topics/"+requestData+"\",\"data\":{\"notification\":{\"body\":\""+message+"\",\"title\":\"" + title +"\",\"confirm\":\"" + confirmLink + "\",\"decline\":\""+ declineLink +"\"}},\"priority\":10}";
+
+  const options = {
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+  };
+
+  function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+          console.log(body);
+      }
+  }
+
+  request(options, callback);
+});
+
+
+
 
 app.use('/api', tokenRouter);
 app.use('/', express.static(__dirname + '/app'));
