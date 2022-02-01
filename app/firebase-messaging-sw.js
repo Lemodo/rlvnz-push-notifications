1ad5685c5513efeb41e8195a32713b57b176ded3
Leo <45721247+Lemodo@users.noreply.github.com>
2022-02-01 11:20:04 +0100

    Update firebase-messaging-sw.js

--- app/firebase-messaging-sw.js
+++ app/firebase-messaging-sw.js
@@ -1,5 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",);
 
 
 
